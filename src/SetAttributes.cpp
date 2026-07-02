d0c1e6605cca17b904b74f558421b4228a8e1a37
Callum Askew <callumaskew@gmail.com>
2026-07-02 14:37:41 +0100

    Added SetAttribute class, used to store informaiton about the rendering and properties of each set

---
+++ src/SetAttributes.cpp
@@ -0,0 +1,4 @@
+#include "SetAttributes.h"
+
+SetAttributes::SetAttributes(float maxIterations, float brightness, float zoom, vec2 center)
+    : maxIterations(maxIterations), brightness(brightness), zoom(zoom), center(center), complexPlanePos(vec2()) {}
