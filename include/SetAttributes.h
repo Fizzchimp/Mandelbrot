6325b04683c9f2db220e83acf318319c46bb365f
Callum Askew <callumaskew@gmail.com>
2026-07-17 17:11:38 +0100

    Added maxZoom as a SetAttribute member, added eforceMaxZoom and enforceSetBoundaries as methods

--- include/SetAttributes.h
+++ include/SetAttributes.h
@@ -8,12 +8,20 @@ struct SetAttributes // Attributes used for rendering both sets
     int maxIterations; // Maximum number of iterations the fragment shader is allowed to calculate
     float brightness; // Relative brightness of the fractal
     float zoom; // Zoom level of the complex plane
+    float maxZoom; // Maximum zoom level allowed
+
     vec2 center; // The position of the center of the rendered object (complex coordinates)
     vec2 complexPlanePos; // Position of the mouse (complex coordinates)
     
     Side side; // Specifies the side of the screen the set is rendered
     // vec2 windowCenter; // The position on the window the center of the set is rendered
 
-    SetAttributes(float maxIterations, float brightness, float zoom, vec2 center, Side side);
+    SetAttributes(float maxIterations, float brightness, float zoom, float maxZoom, vec2 center, Side side);
 
+
+    // Ensure the zoom value is lower than the maximum zoom value
+    void enforceMaxZoom();
+
+    // Ensure the edges of the screen do not go past a certain point on the complex plane
+    void enforceSetBoundaries(int width, int height, bool renderJuliaSet);
 };
--- src/SetAttributes.cpp
+++ src/SetAttributes.cpp
@@ -1,4 +1,38 @@
 #include "SetAttributes.h"
 
-SetAttributes::SetAttributes(float maxIterations, float brightness, float zoom, vec2 center, Side side)
-    : maxIterations(maxIterations), brightness(brightness), zoom(zoom), center(center), complexPlanePos(vec2()), side(side) {}
+SetAttributes::SetAttributes(float maxIterations, float brightness, float zoom, float maxZoom, vec2 center, Side side)
+    : maxIterations(maxIterations), brightness(brightness), zoom(zoom), maxZoom(maxZoom), center(center), complexPlanePos(vec2()), side(side) {}
+
+// Ensure the zoom value is lower than the maximum zoom value
+void SetAttributes::enforceMaxZoom()
+{
+    if (zoom > maxZoom) {zoom = maxZoom;}
+}
+
+void SetAttributes::enforceSetBoundaries(int width, int height, bool renderJuliaSet)
+{
+    // If rendering both sets, the measured window widths are decreased
+    float juliaSetFactor = (1 + renderJuliaSet);
+
+    // Pre calculation because of the screen dimensions
+    float horizontalScale = 1.0f / (1.0f + renderJuliaSet);
+    float verticalScale = 1.0f;
+
+    // Factor in screen dimension inequalities
+    if (width >= height)
+    {
+        verticalScale *= (float)height / width;
+    } 
+    else 
+    {
+        horizontalScale *= (float)width / height;
+    }
+
+    // Calculate if the edge of the screen is past the boundaries and move them back inside
+    if (center.x / horizontalScale - zoom < -maxZoom) {center.x = (-maxZoom + zoom) * horizontalScale;} // Left side of screen
+    else if (center.x / horizontalScale + zoom > maxZoom) {center.x = (maxZoom - zoom) * horizontalScale;} // Right side of screen
+
+    if (center.y / verticalScale - zoom < -maxZoom) {center.y = (-maxZoom + zoom) * verticalScale;} // Bottom of screen
+    else if (center.y / verticalScale + zoom > maxZoom) {center.y = (maxZoom - zoom) * verticalScale;} // Top of screen
+
+}
