53309971dc53dda3ffad655e17b32268e42eed5c
Callum Askew <callumaskew@gmail.com>
2026-07-23 19:10:42 +0100

    removed inverse as the uniform is now set as the inverse

--- res/markervert.glsl
+++ res/markervert.glsl
@@ -11,5 +11,5 @@ uniform mat3 u_translation;
 void main()
 {
 	fragPos = u_translation * vertPos.xyz;
-	gl_Position = mat4(inverse(u_projection) * u_transform) * vertPos; // TODO: MAYBE SET INVERSE AS ITS OWN UNIFORM? (EXCPET ITS NOT THE INVERSE, THE OTHER IS THE INVERSE)
+	gl_Position = mat4(u_projection * u_transform) * vertPos;
 }
